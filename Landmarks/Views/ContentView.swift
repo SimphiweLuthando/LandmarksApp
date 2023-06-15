5927466c4e6a0b8d5f13f59d79cd2179f9d54892
Simphiwe Mbokazi <simphiwem380@gmail.com>
2023-06-15 17:33:47 +0200

    content view refresh

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Simphiwe Mbokazi on 2023/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
